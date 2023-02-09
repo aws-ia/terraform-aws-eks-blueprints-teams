929fa1da9d6642e9cb7c65cee257901a9a4dc70c
Rodrigo Bersa <bersr@amazon.com>
2023-02-09 14:56:56 -0500

    Moving the current state `modules/aws-eks-teams` from `terraform-aws-eks-blueprints` repo.

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "application_teams" {
+  description = "Map of maps of teams to create"
+  type        = any
+  default     = {}
+}
+
+variable "platform_teams" {
+  description = "Map of maps of teams to create"
+  type        = any
+  default     = {}
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "eks_cluster_id" {
+  description = "EKS Cluster name"
+  type        = string
+}
+
+variable "iam_role_permissions_boundary" {
+  description = "ARN of the policy that is used to set the permissions boundary for the IAM role"
+  type        = string
+  default     = null
+}
